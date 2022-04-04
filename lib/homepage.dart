f4ad7ac5ea791d7c9e696dcce8a633afdd437749
excelitai-gunjon <gunjon@excelitai.com>
2022-04-04 11:54:16 +0600

    fix- clippath

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -14,21 +14,21 @@ class _HomePageState extends State<HomePage> {
     return SizedBox(
       height: size.height,
       width: size.width,
-      child: Scaffold(
-        backgroundColor: Colors.white,
-        body: Stack(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height*.4, //255,
-              width: MediaQuery.of(context).size.width,
-              color: Colors.deepPurple,
-              child: Stack(
-                children: [
-                  Align(
-                    alignment: Alignment.topLeft,
-                    child: ClipPath(
-                      clipper: MyCustomClipper(),
-                      child: SafeArea(
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.white,
+          body: Stack(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * .4, //255,
+                width: MediaQuery.of(context).size.width,
+                color: Colors.deepPurple,
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: ClipPath(
+                        clipper: MyCustomClipperTop(),
                         child: Container(
                           width: MediaQuery.of(context).size.width, //200,
                           height:
@@ -38,32 +38,81 @@ class _HomePageState extends State<HomePage> {
                         ),
                       ),
                     ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    Align(
+                      alignment: Alignment.bottomLeft,
+                      child: ClipPath(
+                        clipper: MyCustomClipperBottom(),
+                        child: SafeArea(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width, //200,
+                            height:
+                                150, //MediaQuery.of(context).size.height*.5,//200,
+                            color: Color(0xFFFE4C64),
+                            child: Row(),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {},
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-class MyCustomClipper extends CustomClipper<Path>{
+class MyCustomClipperTop extends CustomClipper<Path> {
   @override
-  Path getClip(Size size){
+  Path getClip(Size size) {
     Path path = Path();
     path.moveTo(0, 0);
     path.lineTo(0, size.height);
     path.quadraticBezierTo(
-      size.width/2,
+      size.width / 2,
       size.height,
       size.width * .7,
-      size.height*0,
+      size.height * 0,
     );
     path.lineTo(size.width * .7, 0); //size.height);
     return path;
   }
+
+  @override
+  bool shouldReclip(covariant CustomClipper oldClipper) {
+    // TODO: implement shouldReclip
+    throw UnimplementedError();
+  }
+}
+
+class MyCustomClipperBottom extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width, size.height);
+    path.lineTo(size.width, size.height / 7);
+    path.quadraticBezierTo(
+      size.width / 2,
+      size.height / 10,
+      size.width / 10,
+      size.height,
+    );
+    path.lineTo(size.width / 7, size.height); //size.height);
+    return path;
+  }
+
   @override
   bool shouldReclip(covariant CustomClipper oldClipper) {
     // TODO: implement shouldReclip
